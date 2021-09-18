4aa031a940586917f849b5e6638d2d4a79ec8522
erkutbas <ebas1@binghamton.edu>
2021-09-18 14:16:22 +0300

    LoadingState enum type added

---
+++ iOS_Bootcamp_Github/Task2/LoadingStates.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingStates.swift
+//  iOS_Bootcamp_Github
+//
+//  Created by Erkut Bas on 18.09.2021.
+//
+
+import Foundation
+
+enum LoadingStates {
+    case loading
+    case done
+}
