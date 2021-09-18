fa3514a71c69e32c0d55000aff922e019f8bcfc2
erkutbas <ebas1@binghamton.edu>
2021-09-18 12:36:53 +0300

    Initial Commit

---
+++ iOS_Bootcamp_Github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Bootcamp_Github
+//
+//  Created by Erkut Bas on 18.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
